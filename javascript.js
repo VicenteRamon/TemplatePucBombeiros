92d042a30a1d4e5db79922a9247fda935d42bd12
Ramon Vicente <vicente.silva.ramon@gmail.com>
2022-05-03 18:19:50 -0300

    login page forms style

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Criar aplicativo
+
+
+// Declaration
+//Functions statement 
+function createPhrases() {
+  document.write("Frase 1")
+  document.write("Frase 2")
+  document.write("Frase 3")
+  document.write("Frase 4")
+}
+
+// execution
+
+
+
